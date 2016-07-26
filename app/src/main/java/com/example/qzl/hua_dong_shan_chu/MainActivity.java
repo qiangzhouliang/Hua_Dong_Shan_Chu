21bf9b2676345f0882120c2adf8f25c7cbdd11ff
Qzl <2538096489@qq.com>
2016-07-26 10:10:53 +0800

    将SwipeLayput设置给adapter

--- app/src/main/java/com/example/qzl/hua_dong_shan_chu/MainActivity.java
+++ app/src/main/java/com/example/qzl/hua_dong_shan_chu/MainActivity.java
@@ -3,12 +3,75 @@ package com.example.qzl.hua_dong_shan_chu;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
+
+    private ListView listView;
+    private List<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        //1 准备数据
+        for (int i = 0; i < 30; i++) {
+            list.add("name - "+i);
+        }
+        listView.setAdapter(new MyAdapter());
+    }
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return list.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null){
+                view = View.inflate(getApplicationContext(),R.layout.adapter_list,null);
+            }
+            ViewHolder holder = ViewHolder.getHolder(view);
+            holder.tv_name.setText(list.get(i));
+            return view;
+        }
+    }
+
+    static class ViewHolder{
+        private TextView tv_name,tv_delete;
+        //构造方法
+        public ViewHolder(View convertView){
+            tv_name = (TextView) convertView.findViewById(R.id.tv_name);
+            tv_delete = (TextView) convertView.findViewById(R.id.tv_delete);
+        }
+        //获取holder
+        public static ViewHolder getHolder(View convertView){
+            ViewHolder holder = (ViewHolder) convertView.getTag();
+            if (holder == null){
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            }
+            return holder;
+        }
     }
 }
--- app/src/main/java/com/example/qzl/hua_dong_shan_chu/SwipeLayout.java
+++ app/src/main/java/com/example/qzl/hua_dong_shan_chu/SwipeLayout.java
@@ -1,7 +1,10 @@
 package com.example.qzl.hua_dong_shan_chu;
 
 import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -15,6 +18,7 @@ public class SwipeLayout extends FrameLayout {
     private int deleteHeight;//delete区域的高度
     private int deleteWidth;//delete区域的宽度
     private int contentWidth;//item内容区域的宽度
+    private ViewDragHelper viewDragHelper;
 
     public SwipeLayout(Context context) {
         super(context);
@@ -31,12 +35,12 @@ public class SwipeLayout extends FrameLayout {
         init();
     }
 
-    private void init(){
-
+    private void init() {
+        viewDragHelper = ViewDragHelper.create(this, callback);
     }
 
     /**
-     *获取子控件
+     * 获取子控件
      */
     @Override
     protected void onFinishInflate() {
@@ -49,6 +53,7 @@ public class SwipeLayout extends FrameLayout {
 
     /**
      * 获取宽高（OnMeasure已经执行完）
+     *
      * @param w
      * @param h
      * @param oldw
@@ -65,6 +70,7 @@ public class SwipeLayout extends FrameLayout {
 
     /**
      * 重写摆放位置
+     *
      * @param changed
      * @param left
      * @param top
@@ -75,7 +81,125 @@ public class SwipeLayout extends FrameLayout {
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
         //重新摆放位置
-        contentView.layout(0,0,contentWidth,deleteHeight);
-        deleteView.layout(contentView.getRight(),0,contentView.getRight()+deleteWidth,deleteHeight);
+        contentView.layout(0, 0, contentWidth, deleteHeight);
+        deleteView.layout(contentView.getRight(), 0, contentView.getRight() + deleteWidth, deleteHeight);
+    }
+
+    /**
+     * 是否拦截事务
+     *
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onInterceptHoverEvent(MotionEvent event) {
+        boolean result = viewDragHelper.shouldInterceptTouchEvent(event);//是否进行处理
+        return result;
+    }
+
+    /**
+     * 处理触摸事件
+     *
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //交给onInterceptHoverEvent处理
+        viewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    private ViewDragHelper.Callback callback = new ViewDragHelper.Callback() {
+        /**
+         * 做伴随移动
+         * @param changedView
+         * @param left
+         * @param top
+         * @param dx
+         * @param dy
+         */
+        @Override
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            super.onViewPositionChanged(changedView, left, top, dx, dy);
+            if (changedView == contentView) {
+                //手动移动deleteView
+                deleteView.layout(deleteView.getLeft() + dx, deleteView.getTop() + dy, deleteView.getRight() + dx, deleteView.getBottom() + dy);
+            } else if (changedView == deleteView) {
+                contentView.layout(contentView.getLeft() + dx, contentView.getTop() + dy, contentView.getRight() + dx, contentView.getBottom() + dy);
+            }
+        }
+
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return child == contentView || child == deleteView;
+        }
+
+        /**
+         * 拖拽范围
+         * @param child
+         * @return
+         */
+        @Override
+        public int getViewHorizontalDragRange(View child) {
+            return deleteWidth;
+        }
+
+        /**
+         * 控制移动
+         * @param child
+         * @param left
+         * @param dx
+         * @return
+         */
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            if (child == contentView) {
+                if (left > 0) left = 0;
+                if (left < -deleteWidth) left = -deleteWidth;
+            } else if (child == deleteView) {
+                if (left > contentWidth) left = contentWidth;
+                if (left < (contentWidth - deleteWidth)) left = (contentWidth - deleteWidth);
+            }
+            return left;
+        }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+            if (contentView.getLeft() < -deleteWidth / 2) {
+                //应该打开
+                open();
+            } else {
+                //应该关闭
+                close();
+            }
+        }
+    };
+
+    /**
+     * 关闭的方法
+     */
+    public void close() {
+        viewDragHelper.smoothSlideViewTo(contentView, 0, contentView.getTop());//平滑移动
+        ViewCompat.postInvalidateOnAnimation(SwipeLayout.this);//刷新
+    }
+
+    /**
+     * 打开的方法
+     */
+    public void open() {
+        viewDragHelper.smoothSlideViewTo(contentView, -deleteWidth, contentView.getTop());//平滑移动
+        ViewCompat.postInvalidateOnAnimation(SwipeLayout.this);//刷新
+    }
+
+    /**
+     * 动画没有完成，继续完成
+     */
+    @Override
+    public void computeScroll() {
+        if (viewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(SwipeLayout.this);//刷新
+        }
     }
 }
